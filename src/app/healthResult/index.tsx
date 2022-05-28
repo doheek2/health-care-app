44cbc33bd8deecf05b7f828d5a3a7635077b062d
rlaebqebq <rlaebqebq@gmail.com>
2022-05-28 13:43:42 +0900

    style : margin 추가

--- src/app/healthResult/index.tsx
+++ src/app/healthResult/index.tsx
@@ -2,10 +2,11 @@ import AverageScoreGraph from './averageScoreGraph';
 import ScorePredictGraph from './scorePredictGraph';
 import CostPredictionGraph from './costPredictionGraph';
 import CompareLastYear from './compareLastYear';
+import styles from './healthResult.module.scss';
 
 const HealthResult = () => {
   return (
-    <section>
+    <section className={styles.sectionWrapper}>
       <CompareLastYear />
       <AverageScoreGraph />
       <ScorePredictGraph />
